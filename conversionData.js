a9dade9e045e99f94a184ef14bf49dd09114b9cd
PARK-SANGGEUN <p3412@nate.com>
2026-02-19 23:13:06 +0900

    Add files via upload

---
+++ conversionData.js
@@ -0,0 +1,7 @@
+const conversionTable = {
+"1.00":{busan:1.45,daejin:1.18},
+"2.00":{busan:3.35,daejin:3.21},
+"3.00":{busan:5.03,daejin:4.94},
+"4.00":{busan:6.67,daejin:6.65},
+"5.00":{busan:9.00,daejin:8.74}
+};
